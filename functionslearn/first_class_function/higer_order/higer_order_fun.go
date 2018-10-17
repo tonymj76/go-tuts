343a50cce00b5746dae787fedfc7306466102894
Anthony <demeyortj@gmail.com>
2018-10-17 21:46:04 +0100

    fmt

--- functionslearn/first_class_function/higer_order/higer_order_fun.go
+++ functionslearn/first_class_function/higer_order/higer_order_fun.go
@@ -2,30 +2,28 @@ package main
 
 import "fmt"
 
-
-func simple(a func(one , two int) int, o, t int) int {
+func simple(a func(one, two int) int, o, t int) int {
 	return a(o, t)
 }
-func test1(){
-	f := func(arg1, arg2 int) int{
+func test1() {
+	f := func(arg1, arg2 int) int {
 		return arg1 + arg2
 	}
 
 	fmt.Println(simple(f, 2, 3))
 }
 
-
-func simple2(a func(arg1, arg2 int) func() int, x,b int ) (func() int) {
+func simple2(a func(arg1, arg2 int) func() int, x, b int) func() int {
 	return a(x, b)
 }
 
-func a(arg1, arg2 int) func() int{
-	return func() int{
+func a(arg1, arg2 int) func() int {
+	return func() int {
 		return (arg1 + arg2)
 	}
 }
 
-func main(){
+func main() {
 	g := simple2(a, 2, 3)
 	fmt.Println(g())
 }
