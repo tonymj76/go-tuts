3a7fad1bc5661fcda1da277694736145a0cd4c9b
Anthony <demeyortj@gmail.com>
2018-10-17 16:26:34 +0100

    how to add polymorphism with interface in go

--- sliceLearn.go
+++ sliceLearn.go
@@ -31,11 +31,78 @@ func (p *path) truncatePath() {
 	holder := bytes.LastIndex(*p, []byte("/"))
 	if holder > 0 {
 		*p = (*p)[:holder] // why put in (*p)
+								//becus to dereference and call a function to it
 	}
 }
-func main() {
+
+func mainfortrunc() {
 	pathName := path("c:/dkd/dkd/we/takeme")
 	pathName.truncatePath()
 	fmt.Printf("%s\n", pathName)
 	fmt.Println(os.Getwd())
 }
+// learning user define type like interface and method set 
+type user struct {
+	username string
+	position string
+}
+
+type admin struct {
+	username string
+	position string
+}
+
+type notice interface{
+	notify()
+}
+
+func (p *user) notify(){
+	fmt.Printf("username is %v and position is %v\n", p.username, p.position)
+}
+
+func (p *admin) notify(){
+	fmt.Printf("username is %v and position is %v\n", p.username, p.position)
+}
+
+func usernotification(n notice) {
+	n.notify() // will expect a location/address
+}
+
+func main() {
+	u :=user{"tony", "manager"}
+	a :=admin{"Mr Cross", "MD"}
+
+	usernotification(&u)
+	usernotification(&a)
+	
+	dict := map[string]int{"one":1}
+	dict2 := make(map[int]string)
+	dict2[2]="two"
+	fmt.Println(dict["one"], dict2[2])
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
