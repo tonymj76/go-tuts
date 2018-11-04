4498c9f57b62c881a2aa6adab8528df17f2170ff
Anthony <demeyortj@gmail.com>
2018-11-04 06:55:39 +0100

    chore(interface) interface or polymorphism in nature

---
+++ readfile/power_interface/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"net/http"
+	"io"
+	"log"
+)
+
+func init() {
+	if len(os.Args) != 1 {
+		fmt.Println("Usage: ./example2 <url> ", os.Args)
+		os.Exit(-1)
+	}
+}
+func check(e error) {
+	if e != nil {
+		log.Fatalln(e)
+	}
+}
+
+func main() {
+	// get response from the web server
+	res, err := http.Get(os.Args[0])
+	check(err)
+
+	// copies from Body to Stdout
+	// os.Stdout implement the io.Writer and res.Body imple io.Reader
+	io.Copy(os.Stdout, res.Body)
+	
+	if e:= res.Body.Close(); e != nil{
+		fmt.Println("did you print? ", e)
+	}
+
+}
