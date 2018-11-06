8202f8204ed94071ccd88f50906a23cdde5331bb
Anthony <demeyortj@gmail.com>
2018-11-06 15:56:27 +0100

    embedded struct type ex1

---
+++ embeding_stuct/ex1/main.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+type namer struct {
+	name string
+}
+
+type people struct {
+	namer
+	age int
+}
+
+type company struct {
+	namer
+	job []string
+}
+
+// giveName is abstract type
+type giveName interface {
+	myName() (string, bool)
+}
+
+func (n namer) myName() (string, bool) {
+	return n.name, len(n.name) > 0
+}
+
+func showName(arg giveName) {
+	if name, ok := arg.myName(); ok {
+		fmt.Println(name)
+	}
+	
+}
+func main() {
+	p1 := people{
+		namer: namer{name: "jerry"},
+		age:   43,
+	}
+
+	c := company{
+		namer: namer{name: "Build2day"},
+		job:   []string{"computer op", "dish washer", "something"},
+	}
+
+	fmt.Println(c.job[2])
+	showName(c)
+	fmt.Println("++++++++++++++++++++++++++++++")
+	showName(p1)
+}
