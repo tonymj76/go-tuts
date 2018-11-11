34425972925c1f52a5598adf84511d829ccdc818
Anthony <demeyortj@gmail.com>
2018-11-11 20:35:49 +0100

    pikes awsome code on fanIn

---
+++ goroutine/channels/fanIn/pike.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+	"fmt"
+)
+
+func main() {
+	/* fi := fanIn(boring("ann"), boring("Ernest"))
+	for i:=0; i<10; i++{
+		fmt.Println(<-fi)
+	} */
+	for n:= range fanIn(boring("ann"), boring("Ernest")){
+		fmt.Println(n)
+	}
+	fmt.Println("You guys are booling")
+}
+
+func boring(msg string) <-chan string {
+	ch := make(chan string)
+	go func(){
+		defer close(ch)
+		for j:=0; ; j++{
+			ch <- fmt.Sprintf("%s  %d",msg, j)
+			time.Sleep(time.Duration(rand.Intn(1e3))* time.Millisecond)
+		}
+		}()
+	return ch
+}
+
+func fanIn(b1, b2 <-chan string) <-chan string {
+	ch := make(chan string)
+	go func() {
+		defer close(ch)
+		for {
+			ch <- <-b1
+		}
+	}()
+	go func() {
+		defer close(ch)
+		for {
+			ch <- <-b2 
+		}
+	}()
+	return ch
+}
