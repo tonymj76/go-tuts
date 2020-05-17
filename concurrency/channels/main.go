fd7204e163e9b10a5b4b4767c9ac994ae55114dc
tonymj76 <demeyortj@gmail.com>
2020-05-17 12:05:11 +0100

    concurrency

---
+++ concurrency/channels/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+func reverse(s string) string {
+	runeStr := []rune(s)
+	for i, j := 0, len(runeStr)-1; i < j; i, j = i+1, j-1 {
+		runeStr[i], runeStr[j] = runeStr[j], runeStr[i]
+	}
+	return string(runeStr)
+}
+
+func main() {
+	dataStream := make(chan string)
+
+	// go func() {
+	// 	fmt.Println(<-dataStream)
+	// }()
+
+	// dataStream <- "Hello Tony "// this just send the data to the channel and main exits without waithing for the goroutine
+
+	go func() {
+		if 0 != 1 { // take this out to remove deadlock
+			return
+		}
+		dataStream <- reverse("Hello Tony")
+	}()
+
+	fmt.Println(<-dataStream) // this blocks main function and wait to receive data
+}
