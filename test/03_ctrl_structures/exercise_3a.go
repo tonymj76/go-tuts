22e122e6c2873c89a51700a72785e950c0e2b0fb
tonymj76 <demeyortj@gmail.com>
2021-02-17 14:58:24 +0100

    variadic function

--- test/03_ctrl_structures/exercise_3a.go
+++ test/03_ctrl_structures/exercise_3a.go
@@ -4,6 +4,18 @@ import (
 	"fmt"
 )
 
+func average(one, two, three float64) float64 {
+	return (one + two + three) / 3
+}
+
+func average2(numbers ...float64) float64 {
+	var sum float64
+	for _, num := range numbers {
+		sum += num
+	}
+	return sum / float64(len(numbers))
+}
+
 func main() {
 	for x, y := 'a', 0; x < 'a'+26; x++ {
 		if y%2 != 0 {
@@ -11,4 +23,5 @@ func main() {
 		}
 		y++
 	}
+	fmt.Println(average2(3.3, 5.2, 3))
 }
