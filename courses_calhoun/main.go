8990e1d9b647ec84ca8cbcb340e677ffe8a3860b
tonymj76 <demeyortj@gmail.com>
2021-12-22 14:46:07 +0000

    reversed

--- courses_calhoun/main.go
+++ courses_calhoun/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	n := []int{1, 3, 5, 8, 4}
@@ -8,7 +11,10 @@ func main() {
 	n = append([]int{7}, n...)
 	fmt.Println(Reverse("demeyor@76"))
 	fmt.Println(ReverseList(n))
-
+	fmt.Println([]rune("0"), string(49))
+	fmt.Println(finMaxSum([]int{4, 2, 1, 7, 8, 1, 2, 8, 1, 0}, 3))
+	fmt.Println()
+	fmt.Println(finSmallestSum(8, []int{4, 2, 2, 7, 8, 1, 2, 8, 10}))
 }
 
 func Reverse(word string) string {
@@ -24,4 +30,32 @@ func ReverseList(list []int) []int {
 		list[i], list[j] = list[j], list[i]
 	}
 	return list
+}
+
+func finMaxSum(n []int, k int) int {
+	maxValue := math.Inf(-1)
+	currentRunningSum := 0
+	for i := 0; i < len(n); i++ {
+		currentRunningSum += n[i]
+		if i >= k-1 {
+			maxValue = math.Max(float64(currentRunningSum), maxValue)
+			currentRunningSum -= n[i-(k-1)]
+		}
+	}
+	return int(maxValue)
+}
+
+func finSmallestSum(target int, list []int) int {
+	minNumber := math.Inf(1)
+	var currListSum int
+	var startingPoint int
+	for endingPoint := 0; endingPoint < len(list); endingPoint++ {
+		currListSum += list[endingPoint]
+		for currListSum >= target {
+			minNumber = math.Min(minNumber, float64(endingPoint-startingPoint+1))
+			currListSum -= list[startingPoint]
+			startingPoint++
+		}
+	}
+	return int(minNumber)
 }
