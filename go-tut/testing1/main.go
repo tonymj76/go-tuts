eeb4abb72bc82ad0115ef015809657b5446af8fd
tonymj76 <demeyortj@gmail.com>
2023-01-05 06:41:22 +0100

    learn channel

--- go-tut/testing1/main.go
+++ go-tut/testing1/main.go
@@ -1,12 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func test1(s chan<- string) {
+	time.Sleep(2 * time.Second)
 	s <- "hello test1"
 }
 
 func test2(s chan<- string) {
+	time.Sleep(5 * time.Second)
 	s <- "hello test2"
 }
 
@@ -16,13 +21,18 @@ func main() {
 	go test1(stream1)
 	go test2(stream2)
 
-	select {
-	case <-stream1:
-		fmt.Println("stream 1 returns")
-	case <-stream2:
-		fmt.Println("stream 2 return")
-		// default:
-		// 	fmt.Println("default returns")
+loop:
+	for {
+		select {
+		case val := <-stream1:
+			fmt.Println("stream 1 returns ", val)
+			break loop
+		case <-stream2:
+			fmt.Println("stream 2 return")
+		default:
+			time.Sleep(1 * time.Second)
+			fmt.Println("default returns")
+		}
 	}
 
 	// s1 := []int{1, 2, 3}
