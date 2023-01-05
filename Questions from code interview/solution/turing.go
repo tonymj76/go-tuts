bad7e856f2fc19e3c0b3835271d3267c8c8578a5
tonymj76 <demeyortj@gmail.com>
2023-01-05 06:42:40 +0100

    findlucky numberx

--- Questions from code interview/solution/turing.go
+++ Questions from code interview/solution/turing.go
@@ -2,44 +2,66 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func findLucky(arr []int) int {
-	result := math.Inf(-1)
-	var windowStart int
-	//hashMap := make(map[int]int)
-	for windowEnd := 0; windowEnd < len(arr); windowEnd++ {
-		count := 0
-		for arr[windowEnd] == arr[windowStart+1] {
-			count++
-			windowStart++
+	hashMap := make(map[int]int)
+	bubble := make([][]int, len(arr))
+	if len(arr) < 2 {
+		return -1
+	}
+	for _, a := range arr {
+		hashMap[a]++
+	}
+	for k, v := range hashMap {
+		bubble[v] = append(bubble[v], k)
+	}
+	for i := len(bubble) - 1; i >= 0; i-- {
+		if len(bubble[i]) != 0 {
+			for _, n := range bubble[i] {
+				if i == n {
+					return n
+				}
+			}
 		}
 	}
-	return int(result)
-}
-
-func groupAnagrams(strs []string) [][]string {
-	hashmap := make(map[[26]int32][]string)
-	var result [][]string
-	for _, str := range strs {
-		var count [26]int32
-
-		for _, s := range str {
-			count[s-'a']++
-		}
-		hashmap[count] = append(hashmap[count], str)
-	}
-	for _, v := range hashmap {
-		result = append(result, v)
-	}
-	return nil
+	return -1
 }
 
 func main() {
-	//nums := []int{1, 2, 2, 3, 3, 3}
-	//
-	//fmt.Println(findLucky(nums))
-	fmt.Println(rune(97))
-	groupAnagrams([]string{"eat", "tea", "tan", "ate", "nat", "bat"})
+	fmt.Println()
+	testCase(findLucky)
+}
+
+func testCase(fn func(arr []int) int) {
+	cases := []struct {
+		arr  []int
+		want int
+	}{
+		{
+			arr:  []int{1, 2, 2, 3, 3, 3},
+			want: 3,
+		},
+		{
+			arr:  []int{2, 2, 3, 4},
+			want: 2,
+		},
+		{
+			arr:  []int{2, 2, 2, 3, 3},
+			want: -1,
+		},
+		{
+			arr:  []int{5},
+			want: -1,
+		},
+	}
+
+	for _, tc := range cases {
+		got := fn(tc.arr)
+		if got == tc.want {
+			fmt.Println("PASS")
+		} else {
+			fmt.Println("FAIL")
+		}
+	}
 }
