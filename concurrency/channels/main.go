5fce4090c4c6109ce7c0bded96f80563ee13707f
tonymj76 <demeyortj@gmail.com>
2023-01-28 22:40:35 +0100

    testing channels

--- concurrency/channels/main.go
+++ concurrency/channels/main.go
@@ -14,6 +14,7 @@ func reverse(s string) string {
 
 func main() {
 	dataStream := make(chan string)
+	alphaStream := make(chan string)
 
 	// go func() {
 	// 	fmt.Println(<-dataStream)
@@ -22,11 +23,27 @@ func main() {
 	// dataStream <- "Hello Tony "// this just send the data to the channel and main exits without waithing for the goroutine
 
 	go func() {
-		if 0 != 1 { // take this out to remove deadlock
-			return
-		}
+		//if 0 != 1 { // take this out to remove deadlock
+		//	return
+		//}
 		dataStream <- reverse("Hello Tony")
 	}()
 
 	fmt.Println(<-dataStream) // this blocks main function and wait to receive data
+	go send(alphaStream)
+	read(alphaStream)
+}
+
+func read(r <-chan string) {
+	for msg := range r {
+		fmt.Println(msg)
+	}
+}
+
+func send(s chan<- string) {
+	for i := 0; i < 10; i++ {
+		al := rune('a' + i)
+		s <- string(al)
+	}
+	close(s)
 }
